dbcfd83badeb3312281d14a9eb7e6c35d728d871
stan podolski <stanp@xlazz.com>
2018-03-09 16:49:17 -0600

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-    name: 'cloudspider:autoform-tags-typeahead',
-    version: '0.7.1',
+    name: 'ipstas:autoform-tags-typeahead',
+    version: '0.7.2',
     summary: 'Provides a autoform tags input with typeahead functionality',
     git: 'https://github.com/Redroest/meteor-autoform-tags-typeahead',
     documentation: 'README.md'
@@ -10,13 +10,13 @@ Package.onUse(function (api) {
     api.versionsFrom('1.1.0.3');
 
     api.use([
-        'mongo',
-        'aldeed:simple-schema@1.3.3',
-        'aldeed:collection2@2.5.0',
-        'aldeed:autoform@5.5.0',
-        'ajduke:bootstrap-tagsinput@0.7.0',
-        'mrt:bootstrap3-typeahead@0.1.1',
-        'templating'
+				'mongo',
+        'ajduke:bootstrap-tagsinput',
+        'mrt:bootstrap3-typeahead',
+        'templating',
+				'blaze',
+				'underscore',
+				'aldeed:autoform'
     ]);
 
     api.addFiles([
--- tags-typeahead.js
+++ tags-typeahead.js
@@ -1,53 +1,54 @@
+AutoForm.addInputType("tags", {
+	template: "afTagsTypeahead",
+	
+	valueIn: function(value) {
+		if(!value) {
+			return '';
+		}
+		console.log('afTagsTypeahead valueIn:', value);
+		return value.join(',');
+	},
+	
+	valueOut: function() {
+		if(!this.val()) {
+			return '';
+		}
+		var tags = this.val().toLowerCase().replace(' ', '').split(',');
 
-
-AutoForm.addInputType("tagsTypeahead", {
-    template: "afTagsTypeahead",
-    valueIn: function(value) {
-        if(!value) {
-            return '';
-        }
-
-        return value.join(',');
-    },
-    valueOut: function() {
-        if(!this.val()) {
-            return '';
-        }
-        var tags = this.val().replace(' ', '').split(',');
-
-        var tagNames = _.map(tags, function(title){
-            var tag = TagsUtil.findOrCreate(title);
-            return tag.title;
-        });
-
-        return tagNames;
-    }
+/*         var tagNames = _.map(tags, function(title){
+				var tag = TagsUtil.findOrCreate(title);
+				return tag.title;
+		}); */
+		var tagNames = tags;
+		console.log('afTagsTypeahead valueOut', tagNames);
+		return tagNames;
+	}
 });
 
 Template.afTagsTypeahead.onRendered(function() {
-    var options = {
-        typeahead: {
-            displayKey: 'title',
-            valueKey: 'title',
-            source: function(str) {
-                return CloudspiderTags.find().map(function(tag){
-                    return tag.title;
-                });
-            }
-        }
-    };
+	var options = {
+/*         typeahead: {
+					displayKey: 'title',
+					valueKey: 'title',
+					source: function(str) {
+							return CloudspiderTags.find().map(function(tag){
+									return tag.title;
+							});
+					}
+			} */
+	};
 
-    //Extend tagsinput options
-    if(this.data.atts && this.data.atts.tagsinput) {
-        _.extend(options, this.data.atts.tagsinput);
-    }
+	//Extend tagsinput options
+	if(this.data.atts && this.data.atts.tagsinput) {
+		_.extend(options, this.data.atts.tagsinput);
+	}
 
-    //Add typeahead options if there are any
-    if(this.data.atts && this.data.atts.typeahead) {
-        options.typeahead = this.data.atts.typeahead;
-    }
+	//Add typeahead options if there are any
+	if(this.data.atts && this.data.atts.typeahead) {
+		options.typeahead = this.data.atts.typeahead;
+	}
 
-    this.$('input').tagsinput(options);
-    this.$('input').attr('data-schema-key', this.data.atts['data-schema-key']);
+	this.$('input').tagsinput(options);
+	this.$('input').attr('data-schema-key', this.data.atts['data-schema-key']);
 });
 
